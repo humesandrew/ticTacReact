703ab265ecfbbc2e4e17e2bf3b70a04a6ee11f55
Andy Humes <andrew.humes@du.edu>
2022-07-07 19:08:00 -0600

    base

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Header from './components/header/Header';
 
 import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<>
+ <Header />
+
+</>
 );
 
 // If you want to start measuring performance in your app, pass a function
