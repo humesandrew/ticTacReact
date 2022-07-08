4f0b5937c54465a205098abfb848869473975df4
Andy Humes <andrew.humes@du.edu>
2022-07-07 20:11:02 -0600

    addtable

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './components/header/Header';
 import AI from './components/ai/AI';
+import Table from './components/table/Table';
 
 import App from './App';
 
@@ -11,6 +12,7 @@ root.render(
 <>
  <Header />
 <AI />
+<Table />
 </>
 );
 
