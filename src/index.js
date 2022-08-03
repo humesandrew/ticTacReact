254886f12eb5e83da92c26309fe88090f00e3ee3
Andy Humes <andrew.humes@du.edu>
2022-08-03 16:27:35 -0600

    style

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './components/header/Header';
-import AI from './components/ai/AI';
+
 import Table from './components/table/Table';
 
 import App from './App';
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <>
  <Header />
- <AI />
+ 
 <Table />
 
 </>
